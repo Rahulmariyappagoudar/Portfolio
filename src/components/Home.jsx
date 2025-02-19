6583a6ae66430fdc452a86f07656f9eb1df79137
Rahul reddy <110841672+Rahulmariyappagoudar@users.noreply.github.com>
2025-02-19 14:37:38 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,48 +1,45 @@
-import React from 'react';
-import { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { name } from '../constants';
-import { motion } from "framer-motion";
 import Background from './Background';
 import Footer from './Footer';
 
 const Home = () => {
+    const ref = useRef(0);
+    const [text, setText] = useState('');
 
-	const ref = useRef(0);
-	const [text, setText] = useState('');
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (ref.current < name.length) {
+                ref.current++;
+                setText((prev) => prev + name[ref.current - 1]);  // Use previous state
+            }
+        }, 500);
+        return () => clearInterval(interval);
+    }, []);  // No dependencies to prevent unwanted resets
 
-	useEffect(() => {
-		const interval = setInterval(() => {
-			if (ref.current < name.length) {
-				ref.current++;
-				setText(() => text + name[ref.current - 1]);
-			}
-		}, 500);
-		return () => clearInterval(interval);
-	}, [text]);
+    return (
+        <div className='area relative z-0 bg-black w-screen h-screen'>
+            {/* Background Circles */}
+            <ul className="circles">
+                {[...Array(10)].map((_, i) => <li key={i}></li>)}
+            </ul>
 
-	return (
-		<div className='area relative z-0 bg-black w-screen h-screen'>
-			<ul class="circles">
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-			</ul>
-			<div className='hero relative h-[calc(100vh)] flex justify-center items-center text-white' id='hero'>
-				<div className='pt-4 h-36 backdrop-blur-sm rounded-3xl'>
-					<h1 className='text-6xl sm:text-7xl font-extrabold mt-2'>Hi, I'm&nbsp;<span className='text-yellow-200 font-extrabold'>{text}</span></h1>
-					<p className='mt-3 text-xl'>I am a passionate Data Analyst and Visualization Enthusiast, exploring the world of data and building impactful insights.</p>
-				</div>      
-			</div>
-			<Footer/>
-		</div>	
-	);
+            {/* Hero Section */}
+            <div className='hero relative h-[calc(100vh)] flex justify-center items-center text-white' id='hero'>
+                <div className='pt-4 min-h-[10rem] rounded-3xl'>
+                    <h1 className='text-6xl sm:text-7xl font-extrabold mt-2'>
+                        Hi, I'm&nbsp;<span className='text-yellow-200 font-extrabold'>{text}</span>
+                    </h1>
+                    <p className='mt-3 text-xl'>
+                        I am a passionate Data Analyst and Visualization Enthusiast, exploring the world of data and building impactful insights.
+                    </p>
+                </div>      
+            </div>
+
+            {/* Footer Component */}
+            <Footer />
+        </div>
+    );
 }
 
 export default Home;
